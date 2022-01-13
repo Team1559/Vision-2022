a1143c94c8e3fba4b13d99858f89ff5e0e3533ea
Rylan Meilutis <44624900+Rylan-Meilutis@users.noreply.github.com>
2022-01-13 18:38:08 -0500

    i is mucho big bad and i'm mad cuz bad

--- Vision2022/2022visionTarget.py
+++ Vision2022/2022visionTarget.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python
-import targetFinder2019
-#import HatchFinder2019
+import targetFinder2022
+# import HatchFinder2019
 import usb
 from socket import *
 import os
 import subprocess
 from datetime import datetime
+
 s = socket(AF_INET, SOCK_DGRAM)
-#address = ("169.254.210.151", 5801)
-#address = ("roborio-1559-frc.local", 5801)
+# address = ("169.254.210.151", 5801)
+# address = ("roborio-1559-frc.local", 5801)
 address = ("10.15.59.2", 5801)
+
 
 def send(data):
     s.sendto(data, address)
+
 
 def sendErrorValues(found, x, y, angle):
     status = 1 if found else 0
@@ -20,20 +23,20 @@ def sendErrorValues(found, x, y, angle):
     print(data)
     send(data)
 
+
 subprocess.check_call(["uvcdynctrl", "-s", "White Balance Temperature, Auto", "0"])
 subprocess.check_call(["uvcdynctrl", "-s", "Brightness", "30"])
 subprocess.check_call(["uvcdynctrl", "-s", "Exposure, Auto", "1"])
 subprocess.check_call(["uvcdynctrl", "-s", "Exposure (Absolute)", "5"])
-camera = usb.USBCamera(1,800,448)
+camera = usb.USBCamera(1, 800, 448)
 
 finder = targetFinder2019.targetFinder(camera)
-#finder = HatchFinder2019.hatchFinder(camera)
+# finder = HatchFinder2019.hatchFinder(camera)
 while 1:
     start = datetime.now()
     result = finder.find()
     end = datetime.now()
-    timeElapsed = end-start
-    #print timeElapsed.total_seconds()
-    #print result
+    timeElapsed = end - start
+    # print timeElapsed.total_seconds()
+    # print result
     sendErrorValues(*result)
-
