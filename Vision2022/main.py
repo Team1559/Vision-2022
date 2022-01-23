932e94eec7fef409dd548be26f21e716578fd1a9
Rylan Meilutis <44624900+Rylan-Meilutis@users.noreply.github.com>
2022-01-23 13:28:03 -0500

    Doesn't work but is close
    gets the frames but doesn't update the output variables

--- Vision2022/main.py
+++ Vision2022/main.py
@@ -9,14 +9,67 @@ import zmq
 import numpy as np
 import ball_finder
 import target_finder
+import ray
+
+ray.init()
 
 
-def main(do_hoop_finder=True, do_ball_finder=True) -> NoReturn:
+is_jetson = False
+cpuArch = ""
+do_hoop_finder = True
+do_ball_finder = True
+hoop_frame = np.zeros(shape=(640, 480, 3))
+ball_frame = np.zeros(shape=(640, 480, 3))
+hoop_result = (0, 0, 0, 0)
+ball_result = (0, 0, 0, 0)
+address = ("10.15.59.2", 5801)
+
+
+def init(do_hoop=True, do_ball=True) -> NoReturn:
+    global is_jetson
+    global cpuArch
+    global do_hoop_finder
+    global do_ball_finder
+
+    do_ball_finder = do_ball
+    do_hoop_finder = do_hoop
+
     is_jetson = False
     cpuArch = platform.uname()[4]
     if cpuArch != "x86_64" and cpuArch != "AMD64":
         is_jetson = True
 
+    if is_jetson:
+        subprocess.check_call(["uvcdynctrl", "-s", "White Balance Temperature, Auto", "0"])
+        subprocess.check_call(["uvcdynctrl", "-s", "Brightness", "30"])
+        subprocess.check_call(["uvcdynctrl", "-s", "Exposure, Auto", "1"])
+        subprocess.check_call(["uvcdynctrl", "-s", "Exposure (Absolute)", "5"])
+
+
+@ray.remote
+def getHoop() -> NoReturn:
+    global hoop_result
+    global hoop_frame
+    hoop_camera = cv2.VideoCapture(1)  # ID should be 1
+    if do_hoop_finder:
+        hoop = target_finder.target_finder(hoop_camera)
+        while True:
+            hoop_result, hoop_frame = hoop.find()
+
+
+@ray.remote
+def getBall() -> NoReturn:
+    global ball_result
+    global ball_frame
+    ball_camera = cv2.VideoCapture(0)  # id should be 0
+    if do_ball_finder:
+        ball = ball_finder.ball_finder(ball_camera)
+        while True:
+            ball_result, ball_frame = ball.find()
+
+
+@ray.remote
+def main() -> NoReturn:
     context = zmq.Context()
     footage_socket = context.socket(zmq.PUB)
     if is_jetson:
@@ -24,78 +77,38 @@ def main(do_hoop_finder=True, do_ball_finder=True) -> NoReturn:
     else:
         footage_socket.connect('tcp://localhost:5555')
 
-    s = socket(AF_INET, SOCK_DGRAM)
     # address = ("169.254.210.151", 5801)
 
     # dns based address ↓↓↓↓
     # address = ("roborio-1559-frc.local", 5801)
     # ip based address ↓↓↓↓↓
-    address = ("10.15.59.2", 5801)
-
-    def send(data: str) -> NoReturn:
-        s.sendto(bytes(data, 'utf-8'), address)
-
-    def send_data(hoop_found: bool, hoop_x: float, hoop_y: float, hoop_angle: float, ball_found: bool, ball_x: float,
-                  ball_y: float, ball_angle: float, wait_for_other_robot: int) -> NoReturn:
-        ball_status = 1 if ball_found else 0
-        hoop_status = 1 if hoop_found else 0
-
-        data = '%3.1f %3.1f %3.1f %3.1f %3.1f %3.1f %d %d %d \n' % (hoop_x, hoop_y, hoop_angle, ball_x, ball_y,
-                                                                    ball_angle, ball_status, hoop_status,
-                                                                    wait_for_other_robot)
-        if not is_jetson:
-            print(data)
-        send(data)
-
-    if is_jetson:
-        subprocess.check_call(["uvcdynctrl", "-s", "White Balance Temperature, Auto", "0"])
-        subprocess.check_call(["uvcdynctrl", "-s", "Brightness", "30"])
-        subprocess.check_call(["uvcdynctrl", "-s", "Exposure, Auto", "1"])
-        subprocess.check_call(["uvcdynctrl", "-s", "Exposure (Absolute)", "5"])
-    if is_jetson:
-        if do_ball_finder:
-            ball_camera = cv2.VideoCapture(1)
-        if do_hoop_finder:
-            hoop_camera = cv2.VideoCapture(2)
-    else:
-        if do_hoop_finder:
-            hoop_camera = cv2.VideoCapture(1)  # ID should be 1
-        if do_ball_finder:
-            ball_camera = cv2.VideoCapture(0)  # id should be 0
-    if do_hoop_finder:
-        hoop = target_finder.target_finder(hoop_camera)
-    if do_ball_finder:
-        ball = ball_finder.ball_finder(ball_camera)
 
     while 1:
 
         try:
             start = datetime.now()
-            if do_hoop_finder:
-                hoop_result, hoop_frame = hoop.find()
-            if do_ball_finder:
-                ball_result, ball_frame = ball.find()
 
-            if not is_jetson and do_hoop_finder and do_ball_finder:
+            if not is_jetson and do_hoop_finder and do_ball_finder and hoop_result is not None and ball_result is not \
+                    None:
                 print(str(hoop_result) + " <-- Hoop, Ball--> " + str (ball_result))
-            elif not is_jetson and do_hoop_finder:
+            elif not is_jetson and do_hoop_finder and hoop_result is not None:
                 print(str(hoop_result) + " <-- Hoop")
-            if not is_jetson and do_ball_finder:
+            if not is_jetson and do_ball_finder and ball_result is not None:
                 print("Ball--> " + str(ball_result))
 
             end = datetime.now()
             timeElapsed = end - start
 
-            if do_ball_finder and do_hoop_finder:
+            if do_ball_finder and do_hoop_finder and hoop_result is not None and ball_result is not None:
                 send_data(*hoop_result, *ball_result, 0)
 
-            elif do_ball_finder and not do_hoop_finder:
+            elif do_ball_finder and not do_hoop_finder and ball_result is not None:
                 send_data(*(False, 0, 0, 0), *ball_result, 0)
 
-            elif not do_ball_finder and do_hoop_finder:
+            elif not do_ball_finder and do_hoop_finder and hoop_result is not None:
                 send_data(*hoop_result, *(False, 0, 0, 0), 0)
 
-            if do_hoop_finder and do_ball_finder:
+            if do_hoop_finder and do_ball_finder and hoop_result is not None and ball_result is not None:
                 # put both frames side by side
                 h1, w1 = hoop_frame.shape[:2]
                 h2, w2 = ball_frame.shape[:2]
@@ -107,24 +120,40 @@ def main(do_hoop_finder=True, do_ball_finder=True) -> NoReturn:
                 encoded, buffer = cv2.imencode('.jpg', vis)
                 footage_socket.send(buffer)
 
-            elif do_ball_finder and not do_hoop_finder:
+            elif do_ball_finder and not do_hoop_finder and ball_result is not None:
                 encoded, buffer = cv2.imencode('.jpg', ball_frame)
                 footage_socket.send(buffer)
 
-            elif not do_ball_finder and do_hoop_finder:
+            elif not do_ball_finder and do_hoop_finder and hoop_result is not None:
                 encoded, buffer = cv2.imencode('.jpg', hoop_frame)
                 footage_socket.send(buffer)
 
         except KeyboardInterrupt:
-            if do_ball_finder:
-                ball_camera.release()
-            if do_hoop_finder:
-                hoop_camera.release()
             cv2.destroyAllWindows()
             print("exiting")
             exit(69420)
 
 
+def send(data: str) -> NoReturn:
+    s = socket(AF_INET, SOCK_DGRAM)
+    s.sendto(bytes(data, 'utf-8'), address)
+
+
+def send_data(hoop_found: bool, hoop_x: float, hoop_y: float, hoop_angle: float, ball_found: bool, ball_x: float,
+              ball_y: float, ball_angle: float, wait_for_other_robot: int) -> NoReturn:
+    ball_status = 1 if ball_found else 0
+    hoop_status = 1 if hoop_found else 0
+
+    data = '%3.1f %3.1f %3.1f %3.1f %3.1f %3.1f %d %d %d \n' % (hoop_x, hoop_y, hoop_angle, ball_x, ball_y,
+                                                                ball_angle, ball_status, hoop_status,
+                                                                wait_for_other_robot)
+    if not is_jetson:
+        print(data)
+    send(data)
+
+
 if __name__ == "__main__":
-    main(do_hoop_finder=True, do_ball_finder=False)
+    init(do_ball=True)
+    ray.get([getHoop.remote(), getBall.remote(), main.remote()])
+
 # it works
